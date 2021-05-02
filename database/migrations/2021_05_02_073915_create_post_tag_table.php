fe72f40277402b9ad23f0ef02908e409581b0a02
Джафар Уразбаев <geniusjmu@gmail.com>
2021-05-03 00:04:24 +0500

    Lab Assignment 9

---
+++ database/migrations/2021_05_02_073915_create_post_tag_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTagTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps ();
+            $table->integer ('post_id');
+            $table->integer ('tag_id');
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('post_tag');
+    }
+}
